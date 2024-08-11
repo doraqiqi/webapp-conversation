348ce0821d9a307c5af70329808640e3d3861c6f
Joe Zhao <yzhb121@aucklanduni.ac.nz>
2024-08-11 23:46:50 +1200

    update

--- config/index.ts
+++ config/index.ts
@@ -5,7 +5,7 @@ export const API_URL = 'http://dify.frwhealthcare.online/v1'
 export const APP_INFO: AppInfo = {
   title: 'FRW Medicine Identification Robot',
   description: 'An AI-driven solution for identifying medicines in our healthcare system.',
-  copyright: '© 2024 FRW Healthcare Company. All rights reserved.',
+  copyright: '2024 FRW Healthcare Company. All rights reserved.',
   privacy_policy: 'http://frwhealthcare.online/privacy-policy',
   default_language: 'en',
 }
