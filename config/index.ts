97bf7a39d612d578dd5728eb616fba5da33704f2
Joe Zhao <yzhb121@aucklanduni.ac.nz>
2024-08-11 22:00:54 +1200

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -12,9 +12,6 @@ export const APP_INFO: AppInfo = {
 
 export const isShowPrompt = false
 export const promptTemplate = 'I want you to act as a javascript console.'
-
 export const API_PREFIX = '/api'
-
 export const LOCALE_COOKIE_NAME = 'locale'
-
 export const DEFAULT_VALUE_MAX_LEN = 48
