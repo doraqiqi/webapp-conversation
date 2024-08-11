0490695e58770f20a8e27a7ee2cd0a3fe766f074
Joe Zhao <yzhb121@aucklanduni.ac.nz>
2024-08-11 21:43:07 +1200

    update env and config

--- config/index.ts
+++ config/index.ts
@@ -3,10 +3,10 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
+  title: 'FRW Medicine Identification Robot',
+  description: 'An AI-driven solution for identifying medicines in our healthcare system.',
+  copyright: '© 2024 FRW Healthcare Company. All rights reserved.',
+  privacy_policy: 'http://frwhealthcare.online/privacy-policy',
   default_language: 'en',
 }
 
